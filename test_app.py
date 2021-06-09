bf4fbceaeda804c7ca0f0eee041dd1b2fa62c377
shariq mahmood <shariq.mahmood@caseware.com>
2021-06-09 16:18:00 -0400

    Fixing the test code

--- test_app.py
+++ test_app.py
@@ -1,22 +1,20 @@
 from app import app
 
-
-def test_hello_world():
+def test_hello_world_get():
     response = app.test_client().get('/')
 
     assert response.status_code == 200
     assert response.data == b'<p>Hello, World</p>'
 
 
-def test_hello_world_2():
+def test_hello_world_post():
     response = app.test_client().post('/')
 
     assert response.status_code == 200
     assert response.data == b'<p>Hello, World</p>'
 
 
-def test_hello_world_1():
-
-    response = app.test_client().get('/',content_type = 'application/json')
+def test_hello_world_get_header():
+    response = app.test_client().get('/', content_type='application/json')
     assert response.status_code == 200
     assert response.data == b'<p>Hello, World</p>'

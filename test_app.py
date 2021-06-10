7e4fbf3d89e724d61f0f5b68ee067f0c69e60cd9
shariq mahmood <shariq.mahmood@caseware.com>
2021-06-09 20:28:51 -0400

    Fixing test case

--- test_app.py
+++ test_app.py
@@ -1,17 +1,19 @@
 from app import app
 
+
 def test_hello_world_get():
     response = app.test_client().get('/')
     assert response.status_code == 200
     assert response.data == b'<p>Hello, World</p>'
 
+
 def test_hello_world_post():
     response = app.test_client().post('/')
-
     assert response.status_code == 200
     assert response.data == b'<p>Hello, World</p>'
+
 
 def test_hello_world_get_header():
-    response = app.test_client().get('/', content_type='application/json')
+    response = app.test_client().get('/', headers={'X-accept-header': 'application/json'})
     assert response.status_code == 200
-    assert response.data == b'<p>Hello, World</p>'
+    assert response.data == b'{"message":"Hello, World"}\n'
